ce52e6ead6cfe30d5e4b7c1b4162fa6f48f7251d
Payalsasmal10 <sasmalpayal@gmail.com>
2024-02-03 16:15:07 +0530

    freelancing page details fetching is going on

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   ]);
 
   return (
-    <div className="App">
+    <div className="">
       <RouterProvider router={router}/>
     </div>
   );
--- src/components/user/freelancer.js
+++ src/components/user/freelancer.js
@@ -1,8 +1,28 @@
 import { useState } from "react";
+import jobDetails from "../mock data/JobDetailsMockData.json";
+import './freelancer.css';
 
 export const Freelancer= () => {
+
     return(
-        <div>Hello User</div>
+        <div>
+        <main>
+            <div>
+
+            {jobDetails.map((jobdetail) => {
+                return(
+
+               <div key={jobdetail.guid} className="card">
+                    <div className="container">
+                        {jobdetail.company}
+                    </div>
+               </div> 
+                );
+            })}
+            </div>
+
+        </main>
+        </div>
     );
 
 };
