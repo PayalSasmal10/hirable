57b2563ef9903f3b8aa162607f8d4b50f233fc51
Payalsasmal10 <sasmalpayal@gmail.com>
2024-02-04 22:17:07 +0530

    fixing css of freelancer profile

--- src/components/profile/freelancerProfile.js
+++ src/components/profile/freelancerProfile.js
@@ -1,14 +1,20 @@
 import React from "react";
+import Header from "../header";
 import "./freelancerProfile.css";
 
 export function FreelancerProfile() {
   return (
-    <div className="card-profile">
-      <div className="content">
-        <h4>Payal Sasmal</h4>
-        <h6>Freelancer</h6>
-        <p>8101689544</p>
-        <p>sasmalpayal@gmail.com</p>
+    <div>
+      <Header />
+      <div className="card-profile-layout">
+        <div className="card-profile">
+          <div className="content-profile">
+            <h4>Payal Sasmal</h4>
+            <h6>Freelancer</h6>
+            <p>8101689544</p>
+            <p>sasmalpayal@gmail.com</p>
+          </div>
+        </div>
       </div>
     </div>
   );
