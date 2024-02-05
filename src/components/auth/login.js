fd1be9bf8121f9eca936cb69a8ba5bca4b21847b
Payalsasmal10 <sasmalpayal@gmail.com>
2024-02-05 23:22:56 +0530

    login form done

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,37 +1,59 @@
-import loginData from "../../data/loginMock.json";
+import freelancerloginData from "../../data/freelancerLoginMock.json";
+import employerloginData from "../../data/employerLoginMockData.json";
 import { Button, Card, Flex, Form, Image, Input, Tabs } from "antd";
 import loginImage from "../../assets/loginImage.svg";
+import { useContext } from "react";
+import { AuthContext } from "../../store/auth-context";
+import { useNavigate } from 'react-router-dom';
 
 export const LoginPage = () => {
   const [form] = Form.useForm();
+  const {token, name, login, firsNameSetter} = useContext(AuthContext);
+  const navigate = useNavigate();
 
-  const onSubmitHandler = (values) => {
-    const user = loginData.find(
-      (user) => user.email === values.email && user.password === values.password
-    );
-    if (user) {
-      console.log("Successfully Login");
-    } else {
-      console.log("Failed to Login");
+  const onSubmitHandler = (values, item) => {
+    console.log("item", item);
+    console.log("values", values);
+    let user="";
+    if(item === "Freelancer"){
+        user = freelancerloginData.find(
+            (user) => user.email === values.email && user.password === values.password
+          );
+          if (user) {
+            console.log("Successfully Login");
+            navigate('/freelancer');
+      
+          } else {
+            console.log("Failed to Login");
+          }
     }
+    if(item === "Employer"){
+        user = employerloginData.find(
+            (user) => user.email === values.email && user.password === values.password
+          );
+          if (user) {
+            console.log("Successfully Login");
+      
+          } else {
+            console.log("Failed to Login");
+          }
+    }
+   
     form.resetFields();
   };
 
-  const loginForm = () => {
+  const loginForm = (item) => {
     return (
       <Form
         name="basic"
         labelCol={{
           span: 8,
         }}
-        wrapperCol={{
-          span: 16,
-        }}
         style={{
           maxWidth: 600,
         }}
         form={form}
-        onFinish={onSubmitHandler}
+        onFinish={(values) => onSubmitHandler(values, item)}
       >
         <Form.Item
           label="Email"
@@ -73,12 +95,14 @@ export const LoginPage = () => {
   };
 
   return (
-    <Flex justify="space-around" align="center">
+    <Flex justify="space-around" align="center" style={{ height: "100vh" }}>
       <Image src={loginImage} preview={{ visible: false }} />
       <Card
         bordered={false}
         style={{
           width: 300,
+          boxShadow:
+            "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px",
         }}
       >
         <Tabs
@@ -88,7 +112,7 @@ export const LoginPage = () => {
             return {
               key: `${item}`,
               label: `${item}`,
-              children: loginForm(),
+              children: loginForm(item),
             };
           })}
         />
